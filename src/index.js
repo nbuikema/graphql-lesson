356d659a5d4d1329dc1f809b184d2f931bdf16cc
nbuikema <46764298+nbuikema@users.noreply.github.com>
2020-10-03 13:28:35 -0500

    refactored carthidden to graphql mutation

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient, gql } from 'apollo-boost';
 
+import { resolvers, typeDefs } from './graphql/resolvers';
 import { store, persistor } from './redux/store';
 
 import './index.css';
@@ -21,7 +22,15 @@ const cache = new InMemoryCache();
 
 const client = new ApolloClient({
   link: httpLink,
-  cache
+  cache,
+  typeDefs,
+  resolvers
+});
+
+client.writeData({
+  data: {
+    cartHidden: true
+  }
 });
 
 ReactDOM.render(
